65a5c3e7552729ac48f07fee01ef7c25f4fdd8b2
kasimlohar <kasimlohar2@gmail.com>
2025-08-25 08:48:34 +0530

    Port change and added .ignore

--- server/index.ts
+++ server/index.ts
@@ -60,11 +60,10 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
+  const host = "0.0.0.0";
+
+  // Removed reusePort (not supported on Windows) to avoid ENOTSUP
+  server.listen(port, host, () => {
     log(`serving on port ${port}`);
   });
 })();
